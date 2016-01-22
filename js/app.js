7580f5be166f17d9e21d022b03a5d1874c238d29
AbdulZaid <aalzaid1@lion.lmu.edu>
2016-01-21 21:11:17 -0800

    Successfully added ui.route instead of ngRoute
    Added ui.route for better state handling over url handling

--- js/app.js
+++ js/app.js
@@ -1,29 +1,33 @@
-// MODULE
-var myApp = angular.module('myApp', ['ngRoute', 'firebase','ui.bootstrap']);
+// Main MODULE... ui.bootstrap is for dropdown menus of bootstrap etc.. router is for states
+var myApp = angular.module('myApp', ['ngRoute', 'firebase','ui.bootstrap','ui.router']);
 
 
+//CONFIGURATION using Ui.router instead of ngRoute
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+	// For any unmatched url, redirect to /state1
+	// $urlRouterProvider.otherwise('views/main.html' )
+	// Now set up the states
+	$stateProvider
+		.state('main', {
+	    	url: "/home",
+	    	controller: 'mainController',
+	    	templateUrl: "views/main.html",
+	        data: {
+	            requireLogin: true
+	        }
+	    })
+	    .state('signup', {
+	    	url: "/signup",
+	    	controller: 'SignupCtrl',
+	    	templateUrl: "views/signup.html"
 
-// //CONFIGURATION
-myApp.config(function ($routeProvider) {
-	$routeProvider  
-		.when('/main', {
-			templateUrl: 'views/main.html',
-			controller: 'mainController',
-			loginRequired: true //
-		})
-		.when('/signup', {
-			templateUrl: 'views/signup.html',
-			controller: 'SignupCtrl',
-		})
-
-		.when('/login', {
-			templateUrl: 'views/login.html',
-			controller: 'LoginCtrl',
-		})
-
-		.otherwise({ redirectTo: '/login' })
-
-});
+	    })
+	    .state('login', {
+	    	url: "/login",
+	    	controller: 'LoginCtrl',
+	    	templateUrl: "views/login.html"
+	    })
+})
 
 //FACTORY
 myApp.factory('Auth', ['$firebaseAuth', function ($firebaseAuth) {
