43f40841735619ba60d79633ab0a4c8b7fb1bc7b
AbdulZaid <aalzaid1@lion.lmu.edu>
2016-01-24 02:48:22 -0800

    successfully redirected user after login, and logout

--- js/app.js
+++ js/app.js
@@ -2,15 +2,44 @@
 var myApp = angular.module('myApp', ['ngRoute', 'firebase','ui.bootstrap','ngAnimate','ui.router','ngMaterial','ngAria','ngMessages']);
 
 // for ui-router
-myApp.run(["$rootScope", "$state", function($rootScope, $state) {
-	$rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
-		// We can catch the error thrown when the $requireAuth promise is rejected
-		// and redirect the user back to the home page
-		if (error === "AUTH_REQUIRED") {
-			console.log("directing for auth")
-			$state.go("login");
-		}
+myApp.run(["$rootScope", "$state","Auth", function($rootScope, $state, Auth) {
+		var ref = new Firebase("https://homeworkmarket.firebaseio.com");
+		var authData = ref.getAuth();
+
+		//keep track of user auth changes
+		Auth.$onAuth(function(authData) {
+		  if (authData) {
+		    console.log("Authenticated with uiiiiiiid:", authData.uid);
+		    authData = authData;
+		    $state.go("main");
+			// event.preventDefault();
+		  } else {
+		    console.log("Clienttttttttt unauthenticated.")
+		    authData = authData;
+		    $state.go("login")
+		   }
+		})
+
+		//redirect to login if not logged in some pages ( successful attempt. )
+		$rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
+			// We can catch the error thrown when the $requireAuth promise is rejected
+			// and redirect the user back to the home page
+			if (error === "AUTH_REQUIRED") {
+				console.log("directing for auth")
+				$state.go("login");
+				event.preventDefault();
+			}
+		});
+
+	//attempt to redirect on state change but not usful for now.
+	$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+		// if (toState.module === 'private' && !authData ) {
+	 //        // If logged in and transitioning to a logged out page:
+	 //        event.preventDefault();
+	 //        $state.go('login');
+	 //    } 
 	});
+
 }]);
 
 //CONFIGURATION using Ui.router instead of ngRoute
@@ -23,6 +52,7 @@ myApp.config(function ($stateProvider, $urlRouterProvider) {
 	    	url: "/home",
 	    	controller: 'mainController',
 	    	templateUrl: "views/main.html",
+	    	module: "private", //custom attributes will be used later.
 	        data: {
 	            requireLogin: true,
 	        },
@@ -49,25 +79,28 @@ myApp.config(function ($stateProvider, $urlRouterProvider) {
 	    .state('signup', {
 	    	url: "/signup",
 	    	controller: 'SignupCtrl',
-	    	templateUrl: "views/signup.html"
+	    	templateUrl: "views/signup.html",
+	    	module: "public",
 
 	    })
 	    .state('login', {
 	    	url: "/login",
 	    	controller: 'LoginCtrl',
-	    	templateUrl: "views/login.html"
+	    	templateUrl: "views/login.html",
+	    	module: "public",
 	    })
 	    .state('editProfile', {
 	    	url: "/editProfile",
 	    	controller: 'ProfileCtrl',
 	    	templateUrl: "views/profile.html",
+	    	module: "private",
 	    	resolve: {
 			    // controller will not be loaded until $requireAuth resolves
 			    // Auth refers to our $firebaseAuth wrapper in the example above
 			    "currentAuth": ["Auth", function(Auth) {
 			      // $requireAuth returns a promise so the resolve waits for it to complete
 			      // If the promise is rejected, it will throw a $stateChangeError (see above)
-			      console.log("requiring auth")
+			      console.log("requiring auth to use edit profile")
 			      return Auth.$requireAuth();
 			    }]
 			}
