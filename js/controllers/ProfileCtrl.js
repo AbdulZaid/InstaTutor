548504b5146fa19179241e71a39808ef82f6e001
AbdulZaid <aalzaid1@lion.lmu.edu>
2016-03-28 02:45:37 -0700

    change of color.

--- js/controllers/ProfileCtrl.js
+++ js/controllers/ProfileCtrl.js
@@ -134,12 +134,12 @@ myApp.controller('ProfileCtrl', ['$scope','Users','Auth','$location','$firebaseA
 
 	    // Configure a dark theme with primary foreground yellow
 	    $mdThemingProvider.theme('default')
-		    .primaryPalette('brown', {
+		    .primaryPalette('indigo', {
 		      'default': '400', // by default use shade 400 from the pink palette for primary intentions
 		      'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
 		      'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
 		      'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
 		    })
-		    .accentPalette('green');
+		    .accentPalette('deep-purple');
 
 	  });
