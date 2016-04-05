b774b38fe3e0b9d350398fd1954c7252f9bd4302
AbdulZaid <aalzaid1@lion.lmu.edu>
2016-04-05 02:47:20 -0700

    indentation fixes

--- js/controllers/AssignmentCtrl.js
+++ js/controllers/AssignmentCtrl.js
@@ -1,7 +1,8 @@
-myApp.controller('AssignmentCtrl', ['$scope','Auth','Users','Posts', '$firebaseArray','$firebaseObject','ngToast','$mdDialog', '$mdMedia', function ($scope, Auth, Users, Posts, $firebaseArray, $firebaseObject, ngToast, $mdDialog, $mdMedia, $location) {
+myApp.controller('AssignmentCtrl', ['$scope', 'Auth', 'Users', 'Posts', '$firebaseArray', '$firebaseObject', 'ngToast', '$mdDialog', '$mdMedia', 
+  function($scope, Auth, Users, Posts, $firebaseArray, $firebaseObject, ngToast, $mdDialog, $mdMedia, $location) {
 
-  var postRef = new Firebase("https://homeworkmarket.firebaseio.com/messages/posts");
-  var authorRef = new Firebase("https://homeworkmarket.firebaseio.com/users");
+    var postRef = new Firebase("https://homeworkmarket.firebaseio.com/messages/posts");
+    var authorRef = new Firebase("https://homeworkmarket.firebaseio.com/users");
 
     // create a synchronized array
     // click on `index.html` above to see it used in the DOM!
@@ -14,162 +15,161 @@ myApp.controller('AssignmentCtrl', ['$scope','Auth','Users','Posts', '$firebaseA
     $scope.status = ' ';
     $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
     $scope.showAdvanced = function(ev, event) {
-        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
         window.postValue = $scope.posts.$getRecord(ev)
         $scope.obj = window.postValue
         console.log($scope.obj)
 
         $mdDialog.show({
-          controller: function DialogController($scope, $mdDialog,items) {
-          $scope.items = items //these are the items inside the object obtained by $getRecord.
-          $scope.closeDialog = function() {
-            $mdDialog.hide();
-        }
-    },
-    templateUrl: 'views/assignment.html',
-    parent: angular.element(document.body),
-      //these are the items inside the object obtained by $getRecord.
-      locals: {
-        items: $scope.obj
-    },
-    targetEvent: ev,
-    clickOutsideToClose:true,
-    fullscreen: useFullScreen,
-})
-        .then(function(answer) {
-          $scope.status = 'You said the information was "' + answer + '".';
-      }, function() {
-          $scope.status = 'You cancelled the dialog.';
-      });
+                controller: function DialogController($scope, $mdDialog, items) {
+                    $scope.items = items //these are the items inside the object obtained by $getRecord.
+                    $scope.closeDialog = function() {
+                        $mdDialog.hide();
+                    }
+                },
+                templateUrl: 'views/assignment.html',
+                parent: angular.element(document.body),
+                //these are the items inside the object obtained by $getRecord.
+                locals: {
+                    items: $scope.obj
+                },
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: useFullScreen,
+            })
+            .then(function(answer) {
+                $scope.status = 'You said the information was "' + answer + '".';
+            }, function() {
+                $scope.status = 'You cancelled the dialog.';
+            });
         $scope.$watch(function() {
-          return $mdMedia('xs') || $mdMedia('sm');
-      }, function(wantsFullScreen) {
-          $scope.customFullscreen = (wantsFullScreen === true);
-      });
+            return $mdMedia('xs') || $mdMedia('sm');
+        }, function(wantsFullScreen) {
+            $scope.customFullscreen = (wantsFullScreen === true);
+        });
     };
 
     $scope.showProposal = function(ev, event) {
         window.postValue = $scope.posts.$getRecord(ev)
         $scope.obj = window.postValue
         $mdDialog.show({
-          controller: function DialogController($scope, $mdDialog, items) {
-        $scope.items = items //these are the items inside the object obtained by $getRecord.
-        $scope.closeDialog = function() {
-          $mdDialog.hide();
-      }
-  },
-  templateUrl: 'views/propose.html',
-  parent: angular.element(document.body),
-  locals: {
-    items: $scope.obj
-},
-targetEvent: ev,
-clickOutsideToClose:true,
-}).then(function(answer) {
-  $scope.status = 'You said the information was "' + answer + '".';
-}, function() {
-  $scope.status = 'You cancelled the dialog.';
-});
-}
-
-$scope.propose = function(authorID, postID) {
-    var tutorID =  $scope.authData.uid
-    var authorID = authorID
-    var postID = postID
-    var time = Firebase.ServerValue.TIMESTAMP
-    var hasNotProposed = Users.hasNotProposed(tutorID)
-
-
-    if(Users.getUserType(tutorID) === "Tutor" && hasNotProposed) {
-      console.log("I'm a tutor")
-
-
-      $scope.author.$loaded(
-        function() {
-          authorPosts = Users.getPosts(authorID) //to get all the posts of a certain author
-          authorCurrentPost = Posts.getSpecificPost(postID)
-          tutorName = Users.getName(tutorID)
-          // console.log(Users.hasN otProposed(tutorID))
-
-          //stores the new  tutor proposal inside the current post list of proposals.
-          authorRef.child(authorID).child("posts").child(authorCurrentPost.$id).child("proposals").push({
-            "tutorID": tutorID,
-            "tutorName": tutorName,
-            "amount": $scope.counterOffer || authorCurrentPost.amount,
-            ".priority": Firebase.ServerValue.TIMESTAMP,
-            "time": time,
-            "message": $scope.proposalMessage || "New propsal",
-            "postID": authorCurrentPost.$id,
-            "postField": authorCurrentPost.field,
-            "assigned": false
-        })
-
-          //get the key of the proposal to store it in the user notifications.
-          authorRef.child(authorID).child("posts").child(authorCurrentPost.$id).child("proposals").orderByKey().limitToLast(1).on('child_added', function(snapshot) {
-            key = snapshot.key()  //get a snapshot of the post's key
+            controller: function DialogController($scope, $mdDialog, items) {
+                $scope.items = items //these are the items inside the object obtained by $getRecord.
+                $scope.closeDialog = function() {
+                    $mdDialog.hide();
+                }
+            },
+            templateUrl: 'views/propose.html',
+            parent: angular.element(document.body),
+            locals: {
+                items: $scope.obj
+            },
+            targetEvent: ev,
+            clickOutsideToClose: true,
+        }).then(function(answer) {
+            $scope.status = 'You said the information was "' + answer + '".';
+        }, function() {
+            $scope.status = 'You cancelled the dialog.';
         });
+    }
 
-          //Stores the proposal as notifications for the student to see anb check.
-          authorRef.child(authorID).child("notifications").child(key).set({
-            "tutorID": tutorID,
-            "tutorName": tutorName,
-            "amount": $scope.counterOffer || authorCurrentPost.amount,
-            ".priority": Firebase.ServerValue.TIMESTAMP,
-            "time": time,
-            "message": $scope.proposalMessage || "new Propsal",
-            "postID": authorCurrentPost.$id,
-            "postField": authorCurrentPost.field,
-            "assigned": false,
-            "viewed": false
-        })
+    $scope.propose = function(authorID, postID) {
+        var tutorID = $scope.authData.uid
+        var authorID = authorID
+        var postID = postID
+        var time = Firebase.ServerValue.TIMESTAMP
+        var hasNotProposed = Users.hasNotProposed(tutorID)
 
-          //checks if the user type is a tutor then store the propsal as his/her.
-          if(Users.getUserType(tutorID) === "Tutor") {
-            authorRef.child(tutorID).child("tutorProposals").child(key).set({
-              "tutorID": tutorID,
-              "tutorName": tutorName,
-              "amount": $scope.counterOffer || authorCurrentPost.amount,
-              ".priority": Firebase.ServerValue.TIMESTAMP,
-              "time": time,
-              "message": $scope.proposalMessage || "new Propsal",
-              "postID": authorCurrentPost.$id,
-              "postField": authorCurrentPost.field,
-              "assigned": false
-          })
+
+        if (Users.getUserType(tutorID) === "Tutor" && hasNotProposed) {
+            console.log("I'm a tutor")
+
+
+            $scope.author.$loaded(
+                function() {
+                    authorPosts = Users.getPosts(authorID) //to get all the posts of a certain author
+                    authorCurrentPost = Posts.getSpecificPost(postID)
+                    tutorName = Users.getName(tutorID)
+                        // console.log(Users.hasN otProposed(tutorID))
+
+                    //stores the new  tutor proposal inside the current post list of proposals.
+                    authorRef.child(authorID).child("posts").child(authorCurrentPost.$id).child("proposals").push({
+                        "tutorID": tutorID,
+                        "tutorName": tutorName,
+                        "amount": $scope.counterOffer || authorCurrentPost.amount,
+                        ".priority": Firebase.ServerValue.TIMESTAMP,
+                        "time": time,
+                        "message": $scope.proposalMessage || "New propsal",
+                        "postID": authorCurrentPost.$id,
+                        "postField": authorCurrentPost.field,
+                        "assigned": false
+                    })
+
+                    //get the key of the proposal to store it in the user notifications.
+                    authorRef.child(authorID).child("posts").child(authorCurrentPost.$id).child("proposals").orderByKey().limitToLast(1).on('child_added', function(snapshot) {
+                        key = snapshot.key() //get a snapshot of the post's key
+                    });
+
+                    //Stores the proposal as notifications for the student to see anb check.
+                    authorRef.child(authorID).child("notifications").child(key).set({
+                        "tutorID": tutorID,
+                        "tutorName": tutorName,
+                        "amount": $scope.counterOffer || authorCurrentPost.amount,
+                        ".priority": Firebase.ServerValue.TIMESTAMP,
+                        "time": time,
+                        "message": $scope.proposalMessage || "new Propsal",
+                        "postID": authorCurrentPost.$id,
+                        "postField": authorCurrentPost.field,
+                        "assigned": false,
+                        "viewed": false
+                    })
+
+                    //checks if the user type is a tutor then store the propsal as his/her.
+                    if (Users.getUserType(tutorID) === "Tutor") {
+                        authorRef.child(tutorID).child("tutorProposals").child(key).set({
+                            "tutorID": tutorID,
+                            "tutorName": tutorName,
+                            "amount": $scope.counterOffer || authorCurrentPost.amount,
+                            ".priority": Firebase.ServerValue.TIMESTAMP,
+                            "time": time,
+                            "message": $scope.proposalMessage || "new Propsal",
+                            "postID": authorCurrentPost.$id,
+                            "postField": authorCurrentPost.field,
+                            "assigned": false
+                        })
+                        ngToast.create({
+                            className: 'success',
+                            content: 'you just propsed to do ' + authorCurrentPost.author
+                        })
+                    }
+
+                    //function to check priorities. 
+                    authorRef.child(authorID).child("notifications").on('child_added', function(snapshot) {
+                        // console.log(snapshot.getPriority());
+                    });
+                })
+        } else {
+            console.log("I'm not a tutor or not authorised")
             ngToast.create({
-              className: 'success',
-              content: 'you just propsed to do ' + authorCurrentPost.author
-          })  
+                className: 'danger',
+                content: 'you are not authorised to propose, or you have already proposed '
+            })
         }
+    }
 
-          //function to check priorities. 
-          authorRef.child(authorID).child("notifications").on('child_added', function(snapshot) { 
-            // console.log(snapshot.getPriority());
-        });
-      })
-} else {
-  console.log("I'm not a tutor")
-  ngToast.create({
-    className: 'danger',
-    content: 'you are not authorised to propose, or you have already proposed '
-})  
-}
-}
+    function DialogController($scope, $mdDialog, items) {
+        $scope.items = items //these are the items inside the object obtained by $getRecord.
+        $scope.hide = function() {
+            $mdDialog.hide();
+        };
+        $scope.cancel = function() {
+            $mdDialog.cancel();
+        };
+        $scope.answer = function(answer) {
+            $mdDialog.hide(answer);
+            console.log(answer)
+        };
 
-function DialogController($scope, $mdDialog, items) {
-    $scope.items = items //these are the items inside the object obtained by $getRecord.
-    $scope.hide = function() {
-      $mdDialog.hide();
-  };
-  $scope.cancel = function() {
-      $mdDialog.cancel();
-  };
-  $scope.answer = function(answer) {
-      $mdDialog.hide(answer);
-      console.log(answer)
-  };
-
-}
+    }
 
 }]);
-
--- js/controllers/PostCtrl.js
+++ js/controllers/PostCtrl.js
@@ -1,4 +1,5 @@
-  myApp.controller('PostCtrl', ['$scope','Auth','Users','$firebaseObject','$firebaseArray','ngToast','Upload', function ($scope, Auth, Users, $firebaseObject, $firebaseArray, ngToast, Upload ) {
+myApp.controller('PostCtrl', ['$scope', 'Auth', 'Users', '$firebaseObject', '$firebaseArray', 'ngToast', 'Upload', 
+  function($scope, Auth, Users, $firebaseObject, $firebaseArray, ngToast, Upload) {
     var messageRef = new Firebase("https://homeworkmarket.firebaseio.com/messages");
     var userRef = new Firebase("https://homeworkmarket.firebaseio.com/users");
     $scope.authData = Auth.$getAuth();
@@ -10,141 +11,153 @@
     $scope.tags = [];
 
     profileObject.$loaded( //to solve the problem with loading data before using it.
-      function() {
-        authorName = Users.getName($scope.authData.uid)
-      });
+        function() {
+            authorName = Users.getName($scope.authData.uid)
+        });
 
 
 
-      // upload later on form submit or something similar
-      $scope.submit = function() {
+    // upload later on form submit or something similar
+    $scope.submit = function() {
         if ($scope.form.file.$valid && $scope.file) {
-          $scope.upload($scope.file);
+            $scope.upload($scope.file);
         }
-      };
+    };
 
-      $scope.upload = function(file) {
-            Upload.upload({
-              url: "https://tutoring-images.s3.amazonaws.com/", //S3 upload url including bucket name
-              method: 'POST',
-              data: {
-                  key: $scope.file, // the key to store the file on S3, could be file name or customized
-                  AWSAccessKeyId: $scope.AWSAccessKeyId,
-                  acl: $scope.acl, // sets the access to the uploaded file in the bucket: private, public-read, ...
-                  policy: $scope.policy, // base64-encoded json policy (see article below)
-                  signature: $scope.signature, // base64-encoded signature based on policy string (see article below)
-                  "Content-Type": $scope.file != '' ? $scope.file : 'application/octet-stream', // content type of the file (NotEmpty)
-                  filename: $scope.file, // this is needed for Flash polyfill IE8-9
-                  file: $scope.file
-              }
-          }).then(function (resp) {
-              console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
-          }, function (resp) {
-              console.log('Error status: ' + resp.data);
-          }, function (evt) {
-              var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-              console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
-          });
-      }
-
+    $scope.upload = function(file) {
+        Upload.upload({
+            url: "https://tutoring-images.s3.amazonaws.com/", //S3 upload url including bucket name
+            method: 'POST',
+            data: {
+                key: $scope.file, // the key to store the file on S3, could be file name or customized
+                AWSAccessKeyId: $scope.AWSAccessKeyId,
+                acl: $scope.acl, // sets the access to the uploaded file in the bucket: private, public-read, ...
+                policy: $scope.policy, // base64-encoded json policy (see article below)
+                signature: $scope.signature, // base64-encoded signature based on policy string (see article below)
+                "Content-Type": $scope.file != '' ? $scope.file : 'application/octet-stream', // content type of the file (NotEmpty)
+                filename: $scope.file, // this is needed for Flash polyfill IE8-9
+                file: $scope.file
+            }
+        }).then(function(resp) {
+            console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+        }, function(resp) {
+            console.log('Error status: ' + resp.data);
+        }, function(evt) {
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+        });
+    }
 
 
 
 
     // COLLAPSE =====================
-    $scope.isCollapsed = false; 
+    $scope.isCollapsed = false;
 
-      $scope.myDate = new Date();
-      $scope.minDate = new Date(
-          $scope.myDate.getFullYear(),
-          $scope.myDate.getDay() - 1,
-          $scope.myDate.getDate()
-      );
+    $scope.myDate = new Date();
+    $scope.minDate = new Date(
+        $scope.myDate.getFullYear(),
+        $scope.myDate.getDay() - 1,
+        $scope.myDate.getDate()
+    );
 
     // Do an if condition to see if the user is a student or not.
     $scope.postMessage = function() {
-      var time = Firebase.ServerValue.TIMESTAMP
-      if(Users.getUserType($scope.authData.uid) === "Student") {
+        var time = Firebase.ServerValue.TIMESTAMP
+        if (Users.getUserType($scope.authData.uid) === "Student") {
 
-        postsRef.push({
-            "authorID": $scope.authData.uid,
-            "author": authorName,
-            "question": $scope.question,
-            "content": $scope.textModel,
-            "field": $scope.field,
-            "dueDate": $scope.dueDate.toJSON(),
-            "amount": $scope.moneyAmount,
-            "assigned": false,
-            "assignedTo": false,
-            "time": time,
-            "tags": $scope.tags,
-            "status": 'open'
-        },
-        function(error) {
-          if (error) {
-            alert("Data could not be saved." + error);
-          } else {
+            postsRef.push({
+                    "authorID": $scope.authData.uid,
+                    "author": authorName,
+                    "question": $scope.question,
+                    "content": $scope.textModel,
+                    "field": $scope.field,
+                    "dueDate": $scope.dueDate.toJSON(),
+                    "amount": $scope.moneyAmount,
+                    "assigned": false,
+                    "assignedTo": false,
+                    "time": time,
+                    "tags": $scope.tags,
+                    "status": 'open'
+                },
+                function(error) {
+                    if (error) {
+                        alert("Data could not be saved." + error);
+                    } else {
+                        ngToast.create({
+                            className: 'success',
+                            content: 'you just posted a new job successfully'
+                        })
+                    }
+                })
+            postsRef.orderByKey().limitToLast(1).on('child_added', function(snapshot) {
+                key = snapshot.key() //get a snapshot of the post's key
+            });
+            userRef.child($scope.authData.uid).child("posts").child(key).set({
+                    "key": key,
+                    "authorID": $scope.authData.uid,
+                    "author": authorName,
+                    "question": $scope.question,
+                    "content": $scope.textModel,
+                    "field": $scope.field,
+                    "dueDate": $scope.dueDate.toJSON(),
+                    "amount": $scope.moneyAmount,
+                    "assigned": false,
+                    "assignedTo": false,
+                    "time": time,
+                    "tags": $scope.tags,
+                    "status": 'open'
+                }) //add new post to user's posts.
+        } else {
+            alert("you not a student.")
             ngToast.create({
-              className: 'success',
-              content: 'you just posted a new job successfully' 
-            })  
-          }
-        })
-        postsRef.orderByKey().limitToLast(1).on('child_added', function(snapshot) {
-          key = snapshot.key()  //get a snapshot of the post's key
-        });
-        userRef.child($scope.authData.uid).child("posts").child(key).set({  
-          "key": key,
-          "authorID": $scope.authData.uid,
-          "author": authorName,
-          "question": $scope.question,
-          "content": $scope.textModel,
-          "field": $scope.field,
-          "dueDate": $scope.dueDate.toJSON(),
-          "amount": $scope.moneyAmount,
-          "assigned": false,
-          "assignedTo": false,
-          "time": time,
-          "tags": $scope.tags,
-          "status": 'open'
-        }) //add new post to user's posts.
-      } else {
-        alert("you not a student.")
-        ngToast.create({
-          className: 'danger',
-          content: 'you are not authorised to post a job. '
-        })  
-      }
+                className: 'danger',
+                content: 'you are not authorised to post a job. '
+            })
+        }
     };
 
-    $scope.fields = [
-      { category: 'Science', name: 'Math' },
-      { category: 'Science', name: 'CMSI' },
-      { category: 'Science', name: 'Biology' },
-      { category: 'Science', name: 'Physics' },
-      { category: 'Engineering', name: 'Electrical Engineering' },
-      { category: 'Engineering', name: 'Mechanical Engineering' },
-      { category: 'Engineering', name: 'Software Engineering' },
-      { category: 'Engineering', name: 'Industrial Engineering' }
-    ];
-  }])
+    $scope.fields = [{
+        category: 'Science',
+        name: 'Math'
+    }, {
+        category: 'Science',
+        name: 'CMSI'
+    }, {
+        category: 'Science',
+        name: 'Biology'
+    }, {
+        category: 'Science',
+        name: 'Physics'
+    }, {
+        category: 'Engineering',
+        name: 'Electrical Engineering'
+    }, {
+        category: 'Engineering',
+        name: 'Mechanical Engineering'
+    }, {
+        category: 'Engineering',
+        name: 'Software Engineering'
+    }, {
+        category: 'Engineering',
+        name: 'Industrial Engineering'
+    }];
+}])
 
-  .config(function($mdDateLocaleProvider) {
-    $mdDateLocaleProvider.formatDate = function(date) {
-      return moment(date).format('YYYY-MM-DD');
-    };
-    // $mdDateLocaleProvider.parseDate = function(dateString) {
-    //   var m = moment(dateString, 'DD/MM/YYYY', true);
-    //   return m.isValid() ? m.toDate() : new Date(NaN);
-    // };
-  })  
-  .config(function($mdThemingProvider) {
+.config(function($mdDateLocaleProvider) {
+        $mdDateLocaleProvider.formatDate = function(date) {
+            return moment(date).format('YYYY-MM-DD');
+        };
+        // $mdDateLocaleProvider.parseDate = function(dateString) {
+        //   var m = moment(dateString, 'DD/MM/YYYY', true);
+        //   return m.isValid() ? m.toDate() : new Date(NaN);
+        // };
+    })
+    .config(function($mdThemingProvider) {
 
         // Configure a dark theme with primary foreground yellow
         $mdThemingProvider.theme('docs-dark', 'default')
-          .primaryPalette('blue')
-          // .dark();
+            .primaryPalette('blue')
+            // .dark();
 
-  });
-
-
+    });
