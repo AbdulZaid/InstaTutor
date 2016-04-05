776d4bd9bb69422ae8166093699ff452db612a5a
AbdulZaid <aalzaid1@lion.lmu.edu>
2016-04-05 02:29:48 -0700

    Checked for image upload/checks is tutor has already proposed

--- js/controllers/AssignmentCtrl.js
+++ js/controllers/AssignmentCtrl.js
@@ -3,88 +3,93 @@ myApp.controller('AssignmentCtrl', ['$scope','Auth','Users','Posts', '$firebaseA
   var postRef = new Firebase("https://homeworkmarket.firebaseio.com/messages/posts");
   var authorRef = new Firebase("https://homeworkmarket.firebaseio.com/users");
 
-	// create a synchronized array
-	// click on `index.html` above to see it used in the DOM!
-	// var query = refOne.orderByChild("timestamp").limitToLast(10);
-	$scope.posts = $firebaseArray(postRef);
-	$scope.authData = Auth.$getAuth()
-  $scope.author = $firebaseObject(authorRef);
-  window.postValue = {}
-  $scope.obj = {}
-  $scope.status = ' ';
-  $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
-  $scope.showAdvanced = function(ev, event) {
-    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
-    window.postValue = $scope.posts.$getRecord(ev)
-    $scope.obj = window.postValue
-    console.log($scope.obj)
+    // create a synchronized array
+    // click on `index.html` above to see it used in the DOM!
+    // var query = refOne.orderByChild("timestamp").limitToLast(10);
+    $scope.posts = $firebaseArray(postRef);
+    $scope.authData = Auth.$getAuth()
+    $scope.author = $firebaseObject(authorRef);
+    window.postValue = {}
+    $scope.obj = {}
+    $scope.status = ' ';
+    $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
+    $scope.showAdvanced = function(ev, event) {
+        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+        window.postValue = $scope.posts.$getRecord(ev)
+        $scope.obj = window.postValue
+        console.log($scope.obj)
 
-    $mdDialog.show({
-      controller: function DialogController($scope, $mdDialog,items) {
+        $mdDialog.show({
+          controller: function DialogController($scope, $mdDialog,items) {
           $scope.items = items //these are the items inside the object obtained by $getRecord.
           $scope.closeDialog = function() {
             $mdDialog.hide();
-          }
-        },
-        templateUrl: 'views/assignment.html',
-        parent: angular.element(document.body),
+        }
+    },
+    templateUrl: 'views/assignment.html',
+    parent: angular.element(document.body),
       //these are the items inside the object obtained by $getRecord.
       locals: {
-      	items: $scope.obj
-      },
-      targetEvent: ev,
-      clickOutsideToClose:true,
-      fullscreen: useFullScreen,
-    })
-    .then(function(answer) {
-      $scope.status = 'You said the information was "' + answer + '".';
-    }, function() {
-      $scope.status = 'You cancelled the dialog.';
-    });
-    $scope.$watch(function() {
-      return $mdMedia('xs') || $mdMedia('sm');
-    }, function(wantsFullScreen) {
-      $scope.customFullscreen = (wantsFullScreen === true);
-    });
-  };
+        items: $scope.obj
+    },
+    targetEvent: ev,
+    clickOutsideToClose:true,
+    fullscreen: useFullScreen,
+})
+        .then(function(answer) {
+          $scope.status = 'You said the information was "' + answer + '".';
+      }, function() {
+          $scope.status = 'You cancelled the dialog.';
+      });
+        $scope.$watch(function() {
+          return $mdMedia('xs') || $mdMedia('sm');
+      }, function(wantsFullScreen) {
+          $scope.customFullscreen = (wantsFullScreen === true);
+      });
+    };
 
-  $scope.showProposal = function(ev, event) {
-    window.postValue = $scope.posts.$getRecord(ev)
-    $scope.obj = window.postValue
-    $mdDialog.show({
-      controller: function DialogController($scope, $mdDialog, items) {
+    $scope.showProposal = function(ev, event) {
+        window.postValue = $scope.posts.$getRecord(ev)
+        $scope.obj = window.postValue
+        $mdDialog.show({
+          controller: function DialogController($scope, $mdDialog, items) {
         $scope.items = items //these are the items inside the object obtained by $getRecord.
         $scope.closeDialog = function() {
           $mdDialog.hide();
-        }
-      },
-      templateUrl: 'views/propose.html',
-      parent: angular.element(document.body),
-      locals: {
-        items: $scope.obj
-      },
-      targetEvent: ev,
-      clickOutsideToClose:true,
-    }).then(function(answer) {
-      $scope.status = 'You said the information was "' + answer + '".';
-    }, function() {
-      $scope.status = 'You cancelled the dialog.';
-    });
-  }
+      }
+  },
+  templateUrl: 'views/propose.html',
+  parent: angular.element(document.body),
+  locals: {
+    items: $scope.obj
+},
+targetEvent: ev,
+clickOutsideToClose:true,
+}).then(function(answer) {
+  $scope.status = 'You said the information was "' + answer + '".';
+}, function() {
+  $scope.status = 'You cancelled the dialog.';
+});
+}
 
-  $scope.propose = function(authorID, postID) {
+$scope.propose = function(authorID, postID) {
     var tutorID =  $scope.authData.uid
     var authorID = authorID
     var postID = postID
     var time = Firebase.ServerValue.TIMESTAMP
-    if(Users.getUserType(tutorID) === "Tutor") {
+    var hasNotProposed = Users.hasNotProposed(tutorID)
+
+
+    if(Users.getUserType(tutorID) === "Tutor" && hasNotProposed) {
       console.log("I'm a tutor")
+
 
       $scope.author.$loaded(
         function() {
           authorPosts = Users.getPosts(authorID) //to get all the posts of a certain author
           authorCurrentPost = Posts.getSpecificPost(postID)
           tutorName = Users.getName(tutorID)
+          // console.log(Users.hasN otProposed(tutorID))
 
           //stores the new  tutor proposal inside the current post list of proposals.
           authorRef.child(authorID).child("posts").child(authorCurrentPost.$id).child("proposals").push({
@@ -97,12 +102,12 @@ myApp.controller('AssignmentCtrl', ['$scope','Auth','Users','Posts', '$firebaseA
             "postID": authorCurrentPost.$id,
             "postField": authorCurrentPost.field,
             "assigned": false
-          })
+        })
 
           //get the key of the proposal to store it in the user notifications.
           authorRef.child(authorID).child("posts").child(authorCurrentPost.$id).child("proposals").orderByKey().limitToLast(1).on('child_added', function(snapshot) {
             key = snapshot.key()  //get a snapshot of the post's key
-          });
+        });
 
           //Stores the proposal as notifications for the student to see anb check.
           authorRef.child(authorID).child("notifications").child(key).set({
@@ -116,7 +121,7 @@ myApp.controller('AssignmentCtrl', ['$scope','Auth','Users','Posts', '$firebaseA
             "postField": authorCurrentPost.field,
             "assigned": false,
             "viewed": false
-          })
+        })
 
           //checks if the user type is a tutor then store the propsal as his/her.
           if(Users.getUserType(tutorID) === "Tutor") {
@@ -130,41 +135,41 @@ myApp.controller('AssignmentCtrl', ['$scope','Auth','Users','Posts', '$firebaseA
               "postID": authorCurrentPost.$id,
               "postField": authorCurrentPost.field,
               "assigned": false
-            })
+          })
             ngToast.create({
               className: 'success',
               content: 'you just propsed to do ' + authorCurrentPost.author
-            })  
-          }
+          })  
+        }
 
           //function to check priorities. 
           authorRef.child(authorID).child("notifications").on('child_added', function(snapshot) { 
-            console.log(snapshot.getPriority());
-          });
-        })
-      } else {
-          console.log("I'm not a tutor")
-          ngToast.create({
-            className: 'danger',
-            content: 'you are not authorised to propose. '
-          })  
-    }
+            // console.log(snapshot.getPriority());
+        });
+      })
+} else {
+  console.log("I'm not a tutor")
+  ngToast.create({
+    className: 'danger',
+    content: 'you are not authorised to propose, or you have already proposed '
+})  
+}
 }
 
 function DialogController($scope, $mdDialog, items) {
     $scope.items = items //these are the items inside the object obtained by $getRecord.
     $scope.hide = function() {
       $mdDialog.hide();
-    };
-    $scope.cancel = function() {
+  };
+  $scope.cancel = function() {
       $mdDialog.cancel();
-    };
-    $scope.answer = function(answer) {
+  };
+  $scope.answer = function(answer) {
       $mdDialog.hide(answer);
       console.log(answer)
-    };
+  };
 
-  }
+}
 
 }]);
 
--- js/services/UsersFac.js
+++ js/services/UsersFac.js
@@ -1,30 +1,50 @@
 myApp.factory('Users', ['$firebaseAuth','$firebaseObject','$firebaseArray', function ($firebaseAuth, $firebaseObject, $firebaseArray) {
-	var usersRef = new Firebase("https://homeworkmarket.firebaseio.com/users");
-	var usersArray = $firebaseArray(usersRef)
+    var usersRef = new Firebase("https://homeworkmarket.firebaseio.com/users");
+    var usersArray = $firebaseArray(usersRef)
 
-	var Users = {
-		getUser: function(uid) {
-			return $firebaseObject(usersRef.child(uid))
-		},
-		getName: function(uid) {
-			return usersArray.$getRecord(uid).name
-		},
-		getUserType: function(uid) {
-			return usersArray.$getRecord(uid).type
-		},
-		getProfile: function(uid) {
-			return usersArray.$getRecord(uid)//for retrieving data in profilePage
-		},
-		getPosts: function(uid) {
-			return $firebaseObject(usersRef.child(uid).child("posts"))
-		},
-		getSpecificPost: function(uid, postID) {
-			return $firebaseObject(usersRef.child(uid).child("posts").child(postID))
-		},
-		getTutorProposlas: function(uid, proposalID) {
-			return $firebaseObject(usersRef.child(uid).child("tutorProposlas").child(proposalID))
-		}
-	}
+    var Users = {
+        getUser: function(uid) {
+            return $firebaseObject(usersRef.child(uid))
+        },
+        getName: function(uid) {
+            return usersArray.$getRecord(uid).name
+        },
+        getUserType: function(uid) {
+            return usersArray.$getRecord(uid).type
+        },
+        getProfile: function(uid) {
+            return usersArray.$getRecord(uid)//for retrieving data in profilePage
+        },
+        getPosts: function(uid) {
+            return $firebaseObject(usersRef.child(uid).child("posts"))
+        },
+        getSpecificPost: function(uid, postID) {
+            return $firebaseObject(usersRef.child(uid).child("posts").child(postID))
+        },
+        // getTutorProposlas: function(uid, proposalID) {
+        //  return $firebaseObject(usersRef.child(uid).child("tutorProposlas").child(proposalID))
+        // },
+        hasNotProposed: function(tutorID) {
+            var notProposed = true
 
-	return  Users;
+            usersRef.child(tutorID).child("tutorProposals").once("value", function(snapshot) {
+                if(snapshot.hasChildren()) {
+                    snapshot.forEach(function(childSnapshot) {
+                      // var zag = childSnapshot.val().tutorID.localeCompare(tutorID)
+                      console.log(childSnapshot.val().tutorID)
+                      if(childSnapshot.val().tutorID.localeCompare(tutorID) == 0) {
+                        notProposed = false
+                    } else {
+                        notProposed = true
+                    }
+                })
+                } else {
+                    notProposed = true
+                }
+            })
+            return notProposed
+        }
+    }
+
+    return  Users;
 }])
